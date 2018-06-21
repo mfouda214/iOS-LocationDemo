2d9d25bd3648edc802181738866e06af4cc94fac
mfouda214 <m.fouda@robone.net>
2018-06-21 21:29:41 +0100

    Getting User Location

--- LocationDemo/ViewController.swift
+++ LocationDemo/ViewController.swift
@@ -7,9 +7,22 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
+    @IBAction func findLocation(_ sender: Any) {
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +34,32 @@ class ViewController: UIViewController {
     }
 
 
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error: " + error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            if (error != nil) {
+                print("Error: " + error!.localizedDescription)
+                return
+            }
+            if placemarks!.count > 0 {
+                let placemark = placemarks![0] as CLPlacemark
+                self.displayLocationDetails(placemark: placemark, location: manager.location!)
+            } else {
+                print("Error retrieving data")
+            }
+        }
+    }
+    
+    func displayLocationDetails(placemark: CLPlacemark, location: CLLocation) {
+        locationManager.stopUpdatingLocation()
+        print("Latitude: \(location.coordinate.latitude)")
+        print("Longitude: \(location.coordinate.longitude)")
+        print("Locality: \(placemark.locality!). Postal Code: \(placemark.postalCode!), Administrative Area: \(placemark.administrativeArea!), Country: \(placemark.country!)")
+    }
+    
 }
 
